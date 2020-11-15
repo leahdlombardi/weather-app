44ddfa99faf856298eb6dc4eeade8532c5b1c8e4
leahdlombardi <leahdlombardi@gmail.com>
2020-11-15 13:06:30 +0000

    Added src folder

---
+++ src/index.js
@@ -0,0 +1,88 @@
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let h2 = document.querySelector("#current-time");
+h2.innerHTML = `${day} ${hours}:${minutes} `;
+
+function getCityTemperature(event) {
+  event.preventDefault();
+  let city = document.querySelector("#new-location").value;
+  searchCity(city);
+}
+
+function displayTemperature(response) {
+  let currentTemperature = Math.round(response.data.main.temp);
+  let currentCity = response.data.name;
+  let displayedTemperature = document.querySelector("#current-temperature");
+  let displayedCity = document.querySelector("#current-location");
+  displayedTemperature.innerHTML = currentTemperature;
+  displayedCity.innerHTML = currentCity;
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findLocation);
+}
+
+function findLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "c65377b33dbe28a83d43ee9cd1615d94";
+  let units = "metric";
+  let apiEndingPoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndingPoint}&lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c65377b33dbe28a83d43ee9cd1615d94";
+  let units = "metric";
+  let apiEndingPoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndingPoint}q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let citySearchEngine = document.querySelector("#city-search");
+citySearchEngine.addEventListener("submit", getCityTemperature);
+
+let currentLocationButton = document.querySelector("#my-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("h3");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("h3");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+
+  temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+}
+
+let fahreinheitLink = document.querySelector("#fahrenheit-change");
+fahreinheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-change");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+searchCity("Manchester");
