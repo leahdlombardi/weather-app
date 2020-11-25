9014c358527f720f1d1ebda52fc7b6a4380dcb73
leahdlombardi <leahdlombardi@gmail.com>
2020-11-25 21:53:17 +0000

    Fixed the celsius and fahrenheit calculations

--- src/index.js
+++ src/index.js
@@ -37,18 +37,18 @@ function getCityTemperature(event) {
 
 
 function displayTemperature(response) {
+  celsiusTemperature = response.data.main.temp;
   let descriptionElement = document.querySelector("#description")
-  let currentTemperature = Math.round(response.data.main.temp);
-  let currentCity = response.data.name;
-  let displayedTemperature = document.querySelector("#current-temperature");
+  let temperatureElement = document.querySelector("#current-temperature");
   let displayedCity = document.querySelector("#current-location");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
   let dateElement = document.querySelector("#current-time");
+  
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   dateElement.innerHTML = formatDate(new Date(), response.data.timezone);
-  displayedTemperature.innerHTML = currentTemperature;
-  displayedCity.innerHTML = currentCity; 
+  displayedCity.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -78,42 +78,13 @@ function findLocation(position) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
-function displayForecast (response){
-  let forecastElement= document.querySelector("#forecast");
-  let forecast = response.data.list[0];
 
-for (let index = 0; index < 5; index++) {
-   forecast= response.data.list[index];
-  tempMax=forecast.main.temp_max;
-   tempMin=forecast.main.temp_min;
-
-  forecastElement.innerHTML += 
-    ` <div class="col">
-      <h3>
-       <div class="hourly-forecast">
-       ${formatHours(forecast.dt * 1000)}
-       </div>
-       </h3>
-      <div class="forecast-icons">
-      <img 
-      src= "src/img/${forecast.weather[0].icon}.png" />
-       </div>
-      <div class="highs-lows">
-     <strong> <span id="temp-max">${Math.round(forecast.main.temp_max)}</span>°
-     </strong> 
-     <span id="temp-min">${Math.round(forecast.main.temp_min)}</span>°
-     </div>
-      </div>`;
-  }
-  }
-
-
-
-function searchCity(city) {
+function searchCity(event) {
+  
   let apiKey = "c65377b33dbe28a83d43ee9cd1615d94";
   let units = "metric";
   let apiEndingPoint = "https://api.openweathermap.org/data/2.5/weather?";
-  let apiUrl = `${apiEndingPoint}q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `${apiEndingPoint}q=${event}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
 
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
@@ -127,25 +98,25 @@ let currentLocationButton = document.querySelector("#my-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("h3");
-  let temperature = temperatureElement.innerHTML;
-  temperature = Number(temperature);
-
-temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
-}
 
 function convertToFahrenheit(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("h3");
+  let temperatureElement = document.querySelector("#current-temperature");
   let temperature = temperatureElement.innerHTML;
   temperature = Number(temperature);
-  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+  temperatureElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
 
 }
 
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  
 
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
 
 
