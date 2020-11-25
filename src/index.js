7cfd2a21547c60d6c6efd871c33622f47b052626
leahdlombardi <leahdlombardi@gmail.com>
2020-11-25 17:14:02 +0000

    Added custom weather icons

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      `src/img/${response.data.weather[0].icon}.png`
     );
     
 }
